3ae595c04265f138cc1e60a746c274bb24543828
Artem Frolov <artem.frolov.aquegen@gmail.com>
2019-06-01 03:11:01 +0300

    no message

--- src/store/actions.tsx
+++ src/store/actions.tsx
@@ -1,6 +1,3 @@
-import {GlobalEventActionTypes} from "./types";
-
-
 export const CHANGE_GLOBAL_EVENT = "CHANGE_GLOBAL_EVENT";
 
 export interface IChangeEventValueAction{
@@ -10,7 +7,7 @@ export interface IChangeEventValueAction{
 
 export const changeEventValue = (newValue: string) : IChangeEventValueAction => {
     return {
-        type: GlobalEventActionTypes.CHANGE_GLOBAL_EVENT,
+        type: CHANGE_GLOBAL_EVENT,
         payload: newValue
     }
 };
--- src/store/types.ts
+++ src/store/types.ts
@@ -2,13 +2,6 @@ export interface GlobalEventState {
     readonly globalEventValue: string
 }
 
-
-export const enum GlobalEventActionTypes {
-    CHANGE_GLOBAL_EVENT = "CHANGE_GLOBAL_EVENT"
-}
-
-
-
 export interface IWelcomeType {
     language: string
     hello: string
